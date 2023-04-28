3c2d1bb5c5eb35191339d01ad224bb6848226cea
Dilmer Valecillos <dilmervst@gmail.com>
2023-04-27 22:45:13 -0600

    Added plane detection example

---
+++ Assets/Scripts/Features/ARPlaneDetection.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+[RequireComponent(typeof(ARPlaneManager))]
+public class ARPlaneDetection : MonoBehaviour
+{
+    private ARPlaneManager planeManager;
+
+    private void Awake()
+    {
+        planeManager = GetComponent<ARPlaneManager>();
+    }
+
+    private void OnEnable()
+    {
+        planeManager.planesChanged += PlanesChanged;    
+    }
+
+    private void OnDisable()
+    {
+        planeManager.planesChanged -= PlanesChanged;
+    }
+
+    private void PlanesChanged(ARPlanesChangedEventArgs obj)
+    {
+        DisplayPlanesChanged("Plane Added", obj.added);
+        DisplayPlanesChanged("Plane Updated", obj.updated);
+        DisplayPlanesChanged("Plane Removed", obj.removed);
+    }
+
+    private void DisplayPlanesChanged(string action, IEnumerable<ARPlane> planes)
+    {
+        foreach (ARPlane plane in planes)
+        {
+            Logger.Instance.LogInfo($"{action}: AR Plane tracckableId: {plane.trackableId}");
+        }
+    }
+}
