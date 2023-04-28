b1a8ad0b3f8e95709ea59780fa0a5307d9297744
Dilmer Valecillos <dilmervst@gmail.com>
2023-04-27 18:06:46 -0600

    Added AR Camera with lighting example

---
+++ Assets/Scripts/Features/ARLightEstimation.cs
@@ -0,0 +1,75 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class ARLightEstimation : MonoBehaviour
+{
+    [Header("UI")]
+    [SerializeField]
+    private TextMeshProUGUI averageBrightnessText;
+
+    [SerializeField]
+    private TextMeshProUGUI colorTemperatureText;
+
+    [SerializeField]
+    private TextMeshProUGUI colorCorrectionText;
+
+    private Light arLight;
+
+    private ARCameraManager arCameraManager;
+
+    private float? brightness;
+
+    private float? colorTemperature;
+
+    private Color? colorCorrection;
+
+    private void Awake()
+    {
+        arLight = FindObjectOfType<Light>();
+        PopulateLightEstimateValues();
+    }
+
+    private void Start()
+    {
+    }
+
+    private void OnEnable()
+    {
+        arCameraManager.frameReceived += FrameReceived;
+    }
+
+    private void OnDisable()
+    {
+        arCameraManager.frameReceived -= FrameReceived;
+    }
+
+    private void FrameReceived(ARCameraFrameEventArgs obj)
+    {
+        Logger.Instance.LogInfo("AR Camera frame received");
+
+        if (obj.lightEstimation.averageBrightness.HasValue)
+        {
+            brightness = obj.lightEstimation.averageBrightness;
+            arLight.intensity = brightness.Value;
+        }
+        if (obj.lightEstimation.averageColorTemperature.HasValue)
+        {
+            colorTemperature = obj.lightEstimation.averageColorTemperature;
+            arLight.colorTemperature = colorTemperature.Value;
+        }
+        if (obj.lightEstimation.colorCorrection.HasValue)
+        {
+            colorCorrection = obj.lightEstimation.colorCorrection;
+            arLight.color = colorCorrection.Value;
+        }
+        PopulateLightEstimateValues();
+    }
+
+    private void PopulateLightEstimateValues()
+    {
+        averageBrightnessText.text = $"Average Brightness: {(!brightness.HasValue ? "Not Set" : brightness.Value)}";
+        colorTemperatureText.text = $"Color Temperature: {(!colorTemperature.HasValue ? "Not Set" : colorTemperature.Value)}";
+        colorCorrectionText.text = $"Color Correction: {(!colorCorrection.HasValue ? "Not Set" : colorCorrection.Value)}";
+    }
+}
