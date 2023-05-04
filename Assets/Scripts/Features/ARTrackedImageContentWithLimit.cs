8a79a4f2fcb72b93838f7eb5b62165cc6e32e790
Dilmer Valecillos <dilmervst@gmail.com>
2023-05-03 22:14:49 -0600

    Added a image tracked content with limit

---
+++ Assets/Scripts/Features/ARTrackedImageContentWithLimit.cs
@@ -0,0 +1,37 @@
+﻿
+using UnityEngine.XR.ARFoundation;
+using UnityEngine;
+
+public class ARTrackedImageContentWithLimit : ARTrackedImageContent
+{
+    [SerializeField]
+    private float removeAfterSecondsOfLimitedTracking = 3.0f;
+
+    public ARTrackedImage ARTrackedImage { get; set; }
+
+    private float timeToLive;
+
+    private void Update()
+    {
+        if (ContentAdded && ARTrackedImage != null && ARTrackedImage.referenceImage.guid != null)
+        {
+            if (ARTrackedImage.trackingState == UnityEngine.XR.ARSubsystems.TrackingState.Limited)
+            {
+                if (timeToLive >= removeAfterSecondsOfLimitedTracking)
+                {
+                    var contentArea = ARTrackedImage.transform.GetChild(1);
+                    foreach (Transform content in contentArea.transform)
+                    {
+                        Destroy(content.gameObject);
+                    }
+                    ContentAdded = false;
+                }
+                timeToLive += Time.deltaTime * 1.0f;
+            }
+            else
+            {
+                timeToLive = 0;
+            }
+        }
+    }
+}
