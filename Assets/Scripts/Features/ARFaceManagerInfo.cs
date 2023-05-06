9b44e1effaf96cf47dc48f9ae980901c7657652a
Dilmer Valecillos <dilmervst@gmail.com>
2023-05-06 11:58:52 -0600

    Added new demo with face tracking

---
+++ Assets/Scripts/Features/ARFaceManagerInfo.cs
@@ -0,0 +1,37 @@
+using System.Linq;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+[RequireComponent(typeof(ARFaceManager))]
+public class ARFaceManagerInfo : MonoBehaviour
+{
+    private ARFaceManager faceManager;
+
+    private void Start()
+    {
+        faceManager = GetComponent<ARFaceManager>();
+    }
+
+    private void OnEnable()
+    {
+        faceManager.facesChanged += FacesChanged;    
+    }
+
+    private void OnDisable()
+    {
+        faceManager.facesChanged -= FacesChanged;
+    }
+
+    private void FacesChanged(ARFacesChangedEventArgs facesChangedEventArgs)
+    {
+        foreach(ARFace face in facesChangedEventArgs.added) 
+        {
+            Logger.Instance.LogInfo($"Face added with trackableId: {face.trackableId} | vertices: {face.vertices.Count()}");
+        }
+
+        foreach (ARFace face in facesChangedEventArgs.removed)
+        {
+            Logger.Instance.LogInfo($"Face removed with trackableId: {face.trackableId}");
+        }
+    }
+}
